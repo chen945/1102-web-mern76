bd310c9f805706f55af72f2047ec222726ee4ded
chen945 <207410076@gms.tku.edu.tw>
2022-04-14 20:55:07 +0800

    P2 connect MongoDB server & P3 Use MongoCompass to create database demo_xx and collection user, enter one document that has your name and sid

--- server_76.js
+++ server_76.js
@@ -1,11 +1,11 @@
-// CommonJS
-// const express = require("express");
-
 //ES6
 import express from "express";
-import { allowedNodeEnvironmentFlags } from "process";
-
 const app = express();
+
+import dontenv from "dotenv";
+dontenv.config();
+
+import connectDB_76 from "./db/connect_76.js";
 
 app.get("/", (req, res) => {
     res.send("Welcome Doris 207410076");
@@ -13,4 +13,16 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const start = async () => {
+    try {
+        await connectDB_76(process.env.MONGO_LOCAL_URL).then(() => {
+            console.log("Connecting to MongoDB");
+        });
+        app.listen(port, () =>
+            console.log(`Server is running on port ${port}`)
+        );
+    } catch (err) {
+        console.log(err);
+    }
+};
+start();
