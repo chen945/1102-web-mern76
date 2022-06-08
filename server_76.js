a3725985bfdde10e463d4ef8130e05a2ff49166c
chen945 <207410076@gms.tku.edu.tw>
2022-06-08 20:24:41 +0800

    w15-p2: client /testproxy to test TestFetchUseProxy_xx.js

--- server_76.js
+++ server_76.js
@@ -7,7 +7,7 @@ dotenv.config();
 import "express-async-errors";
 
 import morgan from "morgan";
-import cors from "cors";
+// import cors from "cors";
 
 // db and authenticateUser
 import connectDB_76 from "./db/connect_76.js";
@@ -20,13 +20,17 @@ import errorHandlerMiddleware_76 from "./middleware/error-handler_76.js";
 if (process.env.NODE.ENV !== "production") {
     app.use(morgan("dev"));
 }
-app.use(cors());
+// app.use(cors()); //若要使用proxy則必須關掉
 app.use(express.json());
 
 app.get("/", (req, res) => {
     // throw new Error("testing for error"); //強制讓它出現錯誤
     // res.send("Welcome Doris 207410076");
     res.json({ msg: "Welcome Doris 207410076" });
+});
+
+app.get("/api/v1", (req, res) => {
+    res.json({ msg: "API v1--- Doris 207410076" });
 });
 app.use("/api/v1/auth_76", authRoutes_76);
 
@@ -36,7 +40,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
     try {
-        await connectDB_76(process.env.MONGO_Local_URL).then(() => {
+        await connectDB_76(process.env.MONGO_URL).then(() => {
             console.log("Connecting to MongoDB");
         });
         app.listen(port, () =>
